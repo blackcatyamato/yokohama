a679c4a3e7f2f372f418653a24c700e4c7e535bc
smartcpu01 <k168009@kccollege.ac.jp>
2017-10-06 11:13:03 +0900

    ページ追加&各種調整(再送)

---
+++ facility.php
@@ -0,0 +1,29 @@
+<?php
+	if($_GET){
+		$name = $_GET["name"];
+	}
+?>
+<!DOCTYPE html>
+<html lang="ja">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="device-width,initial-scale=1">
+		<link rel="stylesheet" href="./content/css/style_common.css">
+		<link rel="stylesheet" href="./content/css/style_facility.css">
+<?php
+		print("<title>".$name."-Yokohama Disaster Prevention</title>");
+?>
+	</head>
+	<body>
+		<header>
+			<h1>
+				YDP
+			</h1>
+		</header>
+		<main>
+			<?php
+				//include "./print_facility_more.php";
+			?>
+		</main>
+	</body>
+</html>
---
+++ region_tsunami_dp_list.php
@@ -0,0 +1,51 @@
+﻿<!DOCTYPE html>
+<html lang="ja">
+	<head>
+		<meta charset="UTF-8">
+		<link rel="stylesheet" href="./content/css/reset.css">
+		<link rel="stylesheet" href="./content/css/style_common.css">
+		<link rel="stylesheet" href="./content/css/style_1.css">
+		<title>津波避難施設-Yokohama Disaster Prevention</title>
+	</head>
+	<body>
+		<header>
+			<h1>
+				YDP
+			</h1>
+		</header>
+		<main id="0">
+			<div class="base_list">
+				<h2>
+					津波避難施設&nbspTsunami&nbspShelter
+				</h2>
+				<div class="ward_link">
+				<ul>
+					<li><a href="#1">鶴見区</a></li>
+					<li><a href="#2">神奈川区</a></li>
+					<li><a href="#3">西区</a></li>
+					<li><a href="#4">中区</a></li>
+					<li><a href="#5">南区</a></li>
+					<li><a href="#6">港南区</a></li>
+					<li><a href="#7">保土ヶ谷区</a></li>
+					<li><a href="#8">旭区</a></li>
+					<li><a href="#9">磯子区</a></li>
+					<li><a href="#10">金沢区</a></li>
+					<li><a href="#11">港北区</a></li>
+					<li><a href="#12">緑区</a></li>
+					<li><a href="#13">青葉区</a></li>
+					<li><a href="#14">都筑区</a></li>
+					<li><a href="#15">戸塚区</a></li>
+					<li><a href="#16">栄区</a></li>
+					<li><a href="#17">泉区</a></li>
+					<li><a href="#18">瀬谷区</a></li>
+				</ul>
+				</div>
+				<div class="print">
+				<?php
+					include "print_1_tsunami.php";
+				 ?>
+				</div>
+			</div>
+		</main>
+	</body>
+</html>
