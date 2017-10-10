66d92543e318dbb922b7bc95b8abe9eab44fcf3f
doragonabe <doragonabekoryu@gmail.com>
2017-10-10 23:20:37 +0900

    マップ更新

--- map.php
+++ map.php
@@ -53,38 +53,12 @@
 		<input type = "submit" value ="送信">
 
 
-
-
-		<table border="1">
-			<tr>
-				<th>住所</th>
-				<td>
-					<input id="address" name="address" type="text" value="">
-				</td>
-			</tr>
-			<tr>
-				<th>緯度</th>
-				<td>
-					<input id="latitude" name="latitude" type="text" value="">
-				</td>
-			</tr>
-			<tr>
-				<th>経度</th>
-				<td>
-					<input id="longitude" name="longitude" type="text" value="">
-				</td>
-			</tr>
-		</table>
-		<input type="button" value="住所から緯度経度を入力する" id="attrLatLng">
-	</form>
-
 	<div id="map"></div>
 
-
+<!--
 	<script>
 /*
 	$(function(){
-
 		function attrLatLngFromAddress(address){
 			var geocoder = new google.maps.Geocoder();
 			geocoder.geocode({'address': address}, function(results, status){
@@ -92,10 +66,9 @@
 					var lat = results[0].geometry.location.lat();
 					var lng = results[0].geometry.location.lng();
 
-
 					// 小数点第六位以下を四捨五入した値を緯度経度にセット、小数点以下の値が第六位に満たない場合は0埋め
 					document.getElementById("latitude").value = (Math.round(lat * 1000000) / 1000000).toFixed(6);
-					document.getElementById("longitude").value = (Math.round(lat * 1000000) / 1000000).toFixed(6);
+					document.getElementById("longitude").value = (Math.round(lng * 1000000) / 1000000).toFixed(6);
 
 				}
 			});
@@ -111,11 +84,17 @@
 
 	});
 */
-
+/*
 
 	var map;
 	$(function(){
-
+		map = new google.maps.Map(document.getElementById('map'), { // #sampleに地図を埋め込む
+			center: { // 地図の中心を指定
+				lat: 34.7019399, // 緯度
+				lng: 135.51002519999997 // 経度
+			},
+			zoom: 19 // 地図のズームを指定
+		});
 
 		var geocoder = new google.maps.Geocoder();
 		var bounds = new google.maps.LatLngBounds();
@@ -125,13 +104,73 @@
 			'東京都千代田区霞が関1-1-1',
 			'東京都千代田区霞が関2-1-3'
 		];
-		geocoder.geocode({'address': address}, function(results, status){
+		geocoder.geocode({'address': addresses}, function(){
 			if(status == google.maps.GeocoderStatus.OK) {
 				var lat = results[0].geometry.location.lat();
 				var lng = results[0].geometry.location.lng();
 			}
 		});
 
+
+	});
+
+	google.load('maps', '3.0', {other_params: 'sensor=false'});
+	  google.setOnLoadCallback(function(){
+	    var map = new google.maps.Map(document.getElementById('map'));
+	    var geocoder = new google.maps.Geocoder();
+	    var bounds = new google.maps.LatLngBounds();
+	    var addresses = [
+	        '東京都千代田区永田町1丁目7-1',
+	        '東京都千代田区霞が関2丁目1番1号',
+	        '東京都千代田区霞が関1-1-1',
+	        '東京都千代田区霞が関2-1-3'
+	    ];
+
+	    addresses.map(function(address, i){
+	    	geocoder.geocode({'address' : address}, function(results, status){
+	            // マーカーの表示
+	            var icon = "http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld="+(i+1)+"|FF0000|000000";
+	            var position = results[0].geometry.location;
+	            var marker = new google.maps.Marker({
+	                icon: icon,
+	                position: position,
+	                map: map
+	            });
+
+	        // 情報ウィンドウの表示
+	        var content = results[0].formatted_address;
+	        var infoWindow = new google.maps.InfoWindow({
+	            content: content,
+	            position: position,
+	            size: new google.maps.Size(50,50)
+	        });
+
+	        // マーカーにマウスを乗せた時に情報ウィンドウを表示
+	        google.maps.event.addListener(marker, 'mouseover', function(){
+	            infoWindow.open(map, marker);
+	        });
+
+	        // マーカーからマウスが離れた時に情報ウィンドウを非表示
+	        google.maps.event.addListener(marker, 'mouseout', function(){
+	            infoWindow.close(map, marker);
+	        });
+
+	        // マーカーが地図の中にいい感じに収まるようにする
+	        bounds.extend(position);
+	        map.fitBounds(bounds);
+
+	        // 住所が1つの場合、寄り過ぎるので zoom を調整
+	        if(addresses.length === 1) map.setZoom(18);
+	      });
+	    });
+	  });
+*/
+
+
+
+/*
+	var addr = ["名古屋市南区","名古屋市東区","名古屋市北区"];
+	$(function(){
 
 
 		map = new google.maps.Map(document.getElementById('map'), { // #sampleに地図を埋め込む
@@ -141,7 +180,76 @@
 			},
 			zoom: 19 // 地図のズームを指定
 		});
+
+
+		var geocoder = new google.maps.Geocoder();
+		for(var i=0; i<addr.length; i++)
+		{
+			geocoder.geocode({address: addr[i]},function(results,status){
+				if(status == google.maps.GeocoderStatus.OK) {
+					/*
+					console.log(results[0].geometry.location.D);
+					console.log(results[0].geometry.location.k);
+					*/
+					/*
+					var lat = results[0].geometry.location.lat();
+					var lng = results[0].geometry.location.lng();
+					document.write(lat);
+				}
+			});
+		}
 	});
+*/
+	</script>
+-->
+	<script>
+		var map;
+		var marker;
+		var geocoder;
+		$(function(){
+			geocoder = new google.maps.Geocoder();
+
+/*
+			geocoder.geocode({
+				'address': '東京都千代田区神田小川町3-28-9' // TAM 東京
+
+*/
+
+
+			var addresses = [
+				'東京都千代田区永田町1丁目7-1',
+				'東京都千代田区霞が関2丁目1番1号',
+				'東京都千代田区霞が関1-1-1',
+				'東京都千代田区霞が関2-1-3'
+			];
+			geocoder.geocode({
+				'address': addresses
+
+			}, function(){
+				if(status == google.maps.GeocoderStatus.OK) {
+					var lat = results[0].geometry.location.lat();
+					var lng = results[0].geometry.location.lng();
+				}
+			});
+
+
+
+
+			}, function(results, status) { // 結果
+				if (status === google.maps.GeocoderStatus.OK) { // ステータスがOKの場合
+					map = new google.maps.Map(document.getElementById('map'), {
+						center: results[0].geometry.location, // 地図の中心を指定
+						zoom: 19 // 地図のズームを指定
+					});
+					marker = new google.maps.Marker({
+						position: results[0].geometry.location, // マーカーを立てる位置を指定
+						map: map // マーカーを立てる地図を指定
+					});
+				} else { // 失敗した場合
+					alert(status);
+				}
+			});
+		});
 	</script>
 	<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBJPq59HWpcrOlRQqN8gCOv9JpgaJlkZCA"></script>
 
