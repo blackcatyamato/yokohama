ac4bf561fcf26dc113b58a21350ee22cdd58b942
doragonabe <doragonabekoryu@gmail.com>
2017-10-10 10:59:23 +0900

    進展なし

--- content/php/table_select.php
+++ content/php/table_select.php
@@ -1,6 +1,6 @@
 <?php
 
-/****仮*************************/
+/****区一覧********************/
 	$stmt = $pdo->query("
 				SELECT
 					Ward,
@@ -21,27 +21,19 @@
 		$ward_content++;
 	}
 
-
-
-
 /******************************/
 
 
-
+/******************************/
 	$stmt = $pdo->prepare("
 				SELECT
-					Type,
-					Definition,
 					Name,
+					Definition,
 					Address,
-					Lat,
-					Lon,
-					Ward,
-					WardCode
+					Ward
 				FROM
 					shelter_jp
 				WHERE
-					Type LIKE '%' AND
 					Ward LIKE ?
 			");
 	$stmt->bindValue(1, $aa, PDO::PARAM_STR);
@@ -50,18 +42,16 @@
 	$i = 1;
 	$content = 0;
 	foreach($stmt as $row){
-		$type[$i] = htmlspecialchars($row["Type"]);
-		$definition[$i] = htmlspecialchars($row["Definition"]);
 		$name[$i] = htmlspecialchars($row["Name"]);
+		$definition[$i] = htmlspecialchars($row["Definition"]);
 		$address[$i] = htmlspecialchars($row["Address"]);
-		$lat[$i] = htmlspecialchars($row["Lat"]);
-		$lon[$i] = htmlspecialchars($row["Lon"]);
 		$ward[$i] = htmlspecialchars($row["Ward"]);
-		$wardCode[$i] = htmlspecialchars($row["WardCode"]);
 
 		$i++;
 		$content++;
 	}
+
+/******************************/
 
 
 ?>
--- map.php
+++ map.php
@@ -16,87 +16,135 @@
 
 	$m_lat = 0;
 	$m_lon = 0;
+
+/*
 	foreach(range(1, $content) as $i):
 		$m_lat += $lat[$i];
 		$m_lon += $lon[$i];
 	endforeach;
+*/
+
 	$m_lat = round($m_lat / $content, 8);
 	$m_lon = round($m_lon / $content, 8);
 ?>
+<!DOCTYPE html>
+<html>
+
+<head>
+	<meta charset="utf-8">
+	<title>aa</title>
+	<style>
+		#map{
+			width: 600px;
+			height: 600px;
+		}
+	</style>
+	<script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+
+</head>
+
+<body>
+	<form method="GET">
+		<select name="ku">
+			<?php foreach(range(1, $ward_content) as $i): ?>
+				<option><?=$ward_list[$i]?></option>
+			<?php endforeach; ?>
+		</select>
+		<input type = "submit" value ="送信">
 
 
 
-<form method="GET">
-	<select name="ku">
-		<?php foreach(range(1, $ward_content) as $i): ?>
-			<option><?=$ward_list[$i]?></option>
-		<?php endforeach; ?>
-	</select>
-	<input type = "submit" value ="送信">
-</form>
 
-<div id="map"></div>
+		<table border="1">
+			<tr>
+				<th>住所</th>
+				<td>
+					<input id="address" name="address" type="text" value="">
+				</td>
+			</tr>
+			<tr>
+				<th>緯度</th>
+				<td>
+					<input id="latitude" name="latitude" type="text" value="">
+				</td>
+			</tr>
+			<tr>
+				<th>経度</th>
+				<td>
+					<input id="longitude" name="longitude" type="text" value="">
+				</td>
+			</tr>
+		</table>
+		<input type="button" value="住所から緯度経度を入力する" id="attrLatLng">
+	</form>
 
-<style>
-	#map{
-		width: 600px;
-		height: 600px;
-	}
-</style>
+	<div id="map"></div>
 
-<script>
-var map;
-var marker = [];
-var infoWindow = [];
-var markerData = [ // マーカーを立てる場所名・緯度・経度
-	<?php foreach(range(1, $content) as $i): ?>
-		{
-			name: "<?=$name[$i]?>",
-			lat: <?=$lat[$i]?>,
-			lng: <?=$lon[$i]?>,
-			/*icon: 'tam.png' // TAM 東京のマーカーだけイメージを変更する*/
-		},
-	<?php endforeach; ?>
-];
 
-function initMap() {
- // 地図の作成
-    var mapLatLng = new google.maps.LatLng({lat: markerData[0]["lat"], lng: markerData[0]["lng"]}); // 緯度経度のデータ作成
-   map = new google.maps.Map(document.getElementById("map"), { // #sampleに地図を埋め込む
-     center: { // 地図の中心を指定
-		lat: <?=$m_lat?>,
-		lng: <?=$m_lon?>
-	},
-      zoom: 13 // 地図のズームを指定
-   });
+	<script>
+/*
+	$(function(){
 
- // マーカー毎の処理
- for (var i = 0; i < markerData.length; i++) {
-        markerLatLng = new google.maps.LatLng({lat: markerData[i]["lat"], lng: markerData[i]["lng"]}); // 緯度経度のデータ作成
-        marker[i] = new google.maps.Marker({ // マーカーの追加
-         position: markerLatLng, // マーカーを立てる位置を指定
-            map: map // マーカーを立てる地図を指定
-       });
+		function attrLatLngFromAddress(address){
+			var geocoder = new google.maps.Geocoder();
+			geocoder.geocode({'address': address}, function(results, status){
+				if(status == google.maps.GeocoderStatus.OK) {
+					var lat = results[0].geometry.location.lat();
+					var lng = results[0].geometry.location.lng();
 
-     infoWindow[i] = new google.maps.InfoWindow({ // 吹き出しの追加
-         content: '<div class="sample">' + markerData[i]["name"] + "</div>" // 吹き出しに表示する内容
-       });
 
-     markerEvent(i); // マーカーにクリックイベントを追加
- }
+					// 小数点第六位以下を四捨五入した値を緯度経度にセット、小数点以下の値が第六位に満たない場合は0埋め
+					document.getElementById("latitude").value = (Math.round(lat * 1000000) / 1000000).toFixed(6);
+					document.getElementById("longitude").value = (Math.round(lat * 1000000) / 1000000).toFixed(6);
 
-   marker[0].setOptions({// TAM 東京のマーカーのオプション設定
-        icon: {
-         url: markerData[0]["icon"]// マーカーの画像を変更
-       }
-   });
-}
+				}
+			});
+		}
 
-// マーカーにクリックイベントを追加
-function markerEvent(i) {
-    marker[i].addListener("click", function() { // マーカーをクリックしたとき
-      infoWindow[i].open(map, marker[i]); // 吹き出しの表示
-  });
-}
-</script>
-<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBJPq59HWpcrOlRQqN8gCOv9JpgaJlkZCA&callback=initMap"></script>
+
+		$('#attrLatLng').click(function(){
+			var address = document.getElementById("address").value;
+			attrLatLngFromAddress(address);
+		});
+
+
+
+	});
+*/
+
+
+	var map;
+	$(function(){
+
+
+		var geocoder = new google.maps.Geocoder();
+		var bounds = new google.maps.LatLngBounds();
+		var addresses = [
+			'東京都千代田区永田町1丁目7-1',
+			'東京都千代田区霞が関2丁目1番1号',
+			'東京都千代田区霞が関1-1-1',
+			'東京都千代田区霞が関2-1-3'
+		];
+		geocoder.geocode({'address': address}, function(results, status){
+			if(status == google.maps.GeocoderStatus.OK) {
+				var lat = results[0].geometry.location.lat();
+				var lng = results[0].geometry.location.lng();
+			}
+		});
+
+
+
+		map = new google.maps.Map(document.getElementById('map'), { // #sampleに地図を埋め込む
+			center: { // 地図の中心を指定
+				lat: 34.7019399, // 緯度
+				lng: 135.51002519999997 // 経度
+			},
+			zoom: 19 // 地図のズームを指定
+		});
+	});
+	</script>
+	<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBJPq59HWpcrOlRQqN8gCOv9JpgaJlkZCA"></script>
+
+</body>
+
+</html>
