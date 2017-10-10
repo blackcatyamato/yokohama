fc6a16f915e815aab065bad8dd6eeef15b065571
doragonabe <doragonabekoryu@gmail.com>
2017-10-11 01:40:04 +0900

    最終更新

--- map.php
+++ map.php
@@ -43,61 +43,60 @@
 	<div id="map"></div>
 
 	<script>
-	$(function(){
-		var map = new google.maps.Map(document.getElementById('map'));
-		var geocoder = new google.maps.Geocoder();
-		var bounds = new google.maps.LatLngBounds();
-		var addresses = [
-			'東京都千代田区永田町1丁目7-1',
-			'東京都千代田区霞が関2丁目1番1号',
-			'東京都千代田区霞が関1-1-1',
-			'東京都千代田区霞が関2-1-3'
-		];
+		$(function(){
+			var map = new google.maps.Map(document.getElementById('map'));
+			var geocoder = new google.maps.Geocoder();
+			var bounds = new google.maps.LatLngBounds();
+			var addresses = [
+				<?php foreach(range(1, $content) as $i): ?>
+					<?="'{$ward[$i]}{$address[$i]}'"?>,
+				<?php endforeach; ?>
+			];
 
-		addresses.map(function(address, i){
-			geocoder.geocode(
-				{
-					"address" : address,
-					"region": "jp"
-				},
-				function(results, status){
+			addresses.map(function(address, i){
+				geocoder.geocode(
+					{
+						"address" : address,
+						"region": "jp"
+					},
+					function(results, status){
 
-					// マーカーの表示
-					var position = results[0].geometry.location;
-					var marker = new google.maps.Marker({
-						position: position,
-						map: map
-					});
+						// マーカーの表示
+						var position = results[0].geometry.location;
+						var marker = new google.maps.Marker({
+							position: position,
+							map: map
+						});
 
-					// 情報ウィンドウの表示
-					var content = results[0].formatted_address;
-					var infoWindow = new google.maps.InfoWindow({
-						content: content,
-						position: position,
-						size: new google.maps.Size(50,50)
-					});
+						// 情報ウィンドウの表示
+						var content = results[0].formatted_address;
+						var infoWindow = new google.maps.InfoWindow({
+							content: content,
+							position: position,
+							size: new google.maps.Size(50,50)
+						});
 
-					// マーカーにマウスを乗せた時に情報ウィンドウを表示
-					google.maps.event.addListener(marker, 'mouseover', function(){
-						infoWindow.open(map, marker);
-					});
+						// マーカーにマウスを乗せた時に情報ウィンドウを表示
+						google.maps.event.addListener(marker, 'mouseover', function(){
+							infoWindow.open(map, marker);
+						});
 
-					// マーカーからマウスが離れた時に情報ウィンドウを非表示
-					google.maps.event.addListener(marker, 'mouseout', function(){
-						infoWindow.close(map, marker);
-					});
+						// マーカーからマウスが離れた時に情報ウィンドウを非表示
+						google.maps.event.addListener(marker, 'mouseout', function(){
+							infoWindow.close(map, marker);
+						});
 
-					// マーカーが地図の中にいい感じに収まるようにする
-					bounds.extend(position);
-					map.fitBounds(bounds);
+						// マーカーが地図の中にいい感じに収まるようにする
+						bounds.extend(position);
+						map.fitBounds(bounds);
 
-					// 住所が1つの場合、寄り過ぎるので zoom を調整
-					if(addresses.length === 1) map.setZoom(18);
+						// 住所が1つの場合、寄り過ぎるので zoom を調整
+						if(addresses.length === 1) map.setZoom(18);
 
-				}
-			);
+					}
+				);
+			});
 		});
-	});
 	</script>
 
 
