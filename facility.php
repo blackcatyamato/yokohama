fe839be3ffcdb6293889a50b53a9dd29439ff20d
smartcpu01 <k168009@kccollege.ac.jp>
2017-10-20 10:58:08 +0900

    いろいろ変更2

--- facility.php
+++ facility.php
@@ -12,9 +12,7 @@
 		<link rel="stylesheet" href="./content/css/style_common.css">
 		<link rel="stylesheet" href="./content/css/style_facility.css">
 		<link rel="stylesheet" href="./content/css/style_header.css">
-<?php
-		print("<title>".$name."-Yokohama Disaster Prevention</title>");
-?>
+		<title>施設詳細-Yokohama Disaster Prevention</title>
 	</head>
 		<?php
 			$page = "施設詳細";
@@ -30,10 +28,12 @@
 				$stmt = $dbh->query($sql);
 
 				$result = $stmt->fetch(PDO::FETCH_ASSOC);
-				print("<div class=\"ft_box\">");
+				print("<div class=\"fa_box\">");
 				print("<h3>".$result["Name"]."</h3>");
+				print("<div class=\"fa_inner\">");
 				print("<h4>住所</h4>");
 				print("<p>".$result["Address"]."</p>");
+				print("</div>");
 				print("</div>");
 			}catch(PDOException $e){
 				print("Connection failed:".$e->getMessage());
