5e66fb2dde28568ebd3d8ab8ec3f581651269998
doragonabe <doragonabekoryu@gmail.com>
2017-10-20 12:33:11 +0900

    test

--- index.php
+++ index.php
@@ -47,7 +47,7 @@
 						<h4>案内</h4>
 						<ul>
 							<li class="Tunami">
-								<a href="#">津波避難
+								<a href="<?=$path?>map.php?type=tsunami">津波避難
 									<img src="<?=$path?>content/img/tsunami2.png">
 								</a>
 							</li>
@@ -62,7 +62,7 @@
 								</a>
 							</li>
 							<li class="Tbousai">
-								<a href="<?=$path?>map.php">地域防災拠点
+								<a href="<?=$path?>map.php?type=shelter">地域防災拠点
 									<img src="<?=$path?>content/img/tsunami2.png">
 								</a>
 							</li>
